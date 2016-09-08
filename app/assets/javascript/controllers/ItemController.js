e249e40ce6d7004e85678babf3f5f88745a05129
Solbrad <solbrad@gmail.com>
2016-09-08 22:01:32 +1200

    removed old code

--- app/assets/javascript/controllers/ItemController.js
+++ app/assets/javascript/controllers/ItemController.js
@@ -12,20 +12,4 @@ angular.module('FeedMe').controller('ItemController', function (Item, Cat, $scop
     $scope.cats = result;
   });
 
-
-  $scope.checkExpiry = function(expDate){
-    var d = new Date();
-    var today = new Date(d.getFullYear(),d.getMonth()+1,d.getDate());
-    var exp = expDate.split("-");
-    var expDate = new Date(exp[0],exp[1],exp[2]);
-    
-    if(today.getTime() > expDate.getTime()){
-      return 'critical';
-    }else if(today.getTime() == expDate.getTime()){
-      return 'warn';
-    }else{
-      return 'okay';
-    }
-  }
-
 });
